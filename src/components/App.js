3de72776a30f9c76eeddbf6afab3056b168f12b4
LaupWing <loc_strict@hotmail.com>
2022-04-19 14:21:45 +0200

    loadWeb3

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,18 @@ import Main from './Main'
 
 
 class App extends Component {
+   async loadWeb3(){
+      if(window.ethereum){
+         window.web3 = new Web3(window.ethereum)
+         await window.ethereum.enable()
+      }
+      else if(window.web3){
+         window.web3 = new Web3(window.web3.currentProvider)
+      }
+      else{
+         window.alert('Non-Ethereum browser detected. You should consider trying metamask')
+      }
+   }
 
    constructor(props) {
       super(props)
